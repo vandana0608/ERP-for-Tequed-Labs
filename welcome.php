108ea889b76d9aac9a99bfd7c6ef6b51fdcbcbe7
vandana0608 <vandu.tulip@gmail.com>
2020-01-16 14:55:32 +0530

    commit

--- welcome.php
+++ welcome.php
@@ -1,5 +1,12 @@
 <html>
+    <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</head>
 <body>
+    
 <?php 
 $con=mysqli_connect("localhost","root","","login");     //connect through DB through APache server (website,username,password,DB name)
 if(mysqli_connect_errno()){                            //to check if the connection to server is done
@@ -20,11 +27,13 @@ if($count == 1){      //exact match to the number of rows with the same name wit
     $_SESSION['login_user']=$username;
     header("location: index.html");
 }
-else{
-    echo"<center><h3 style='color:red';>Your login name or password is incorrect";
+else{?>
+   <!-- echo"<center><h3 style='color:red';>Your login name or password is incorrect"; -->
+  <script>  window.alert("Invalid Username & password.");
+            window.location = "login.html";
+  </script>
+<?php
 }
-
-
 ?>
 
 </body>
